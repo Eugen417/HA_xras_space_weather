87415a7a09d686c442a51747de0d538417a8cfe1
Eugen417 <eugen417@gmail.com>
2026-04-20 09:21:39 +0300

    Update const.py

--- custom_components/xras_space_weather/const.py
+++ custom_components/xras_space_weather/const.py
@@ -7,7 +7,7 @@ URL_HTML_AURORA = "https://xras.ru/aurora.html"
 URL_HTML_FLARES = "https://xras.ru/sun_flares.html"
 UPDATE_INTERVAL_MINUTES = 30
 
-# Полный список городов представленных в сервисе (https://xras.ru) для: России, Европы, Канады, США, Индии, Океании
+# Полный список городов представленных в сервисе (https://xras.ru) для: России, СНГ, Европы, Канады, США, Индии, Океании
 CITIES = {
     "abakan": {"id": "QYPM", "name": "Абакан"},
     "anadyr": {"id": "SSWT", "name": "Анадырь"},
